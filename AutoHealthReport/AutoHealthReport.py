81418082ed105c304654f6218aaa7315a5bf9083
Pengwei-Chen <61215150+Pengwei-Chen@users.noreply.github.com>
2022-03-21 23:30:04 +0800

    Website update

--- AutoHealthReport/AutoHealthReport.py
+++ AutoHealthReport/AutoHealthReport.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
+# from selenium.webdriver.support.select import Select
 import time
 import requests
 import re
@@ -10,38 +10,38 @@ import sys
 import subprocess
 
 ##########################
-#Enter your username here#
+# Enter your username here #
 username = "3190110642"
 
-#Enter your password here#
+# Enter your password here #
 password = "123456789"
 
-#Enter your location here (optional)#
-province = ""
-city = ""
-area = ""
+# Enter your location here (optional) #
+# province = "浙江省"
+# city = "嘉兴市"
+# area = "海宁市"
 
-#*sfyxjzxgym是否意向接种
-#*sfbyjzrq是否是不宜接种人群
-#*jzxgymqk当前接种情况
-#sffrqjwdg今日是否因发热请假未到岗（教职工）或未返校（学生）
-#sfqtyyqjwdg今日是否因发热外的其他原因请假未到岗（教职工）或未返校（学生）
-#tw今日是否有发热症状（高于37.2 ℃）
-#sfyqjzgc今日是否被当地管理部门要求在集中隔离点医学观察
-#sfcyglq今日是否居家隔离观察（居家非隔离状态填否）
-#sfcxzysx是否有任何与疫情相关的，值得注意的情况
-#sfsqhzjkk是否已经申领校区所在地健康码
-#sqhzjkkys今日申领校区所在地健康码的颜色
-#*sfzx今日是否在校
-#sfzgn所在地点
-#no need to check for 所在地点（请打开手机位置功能，并在手机权限设置中选择允许访问位置信息）
-#*sfymqjczrj本人家庭成员(包括其他密切接触人员)是否有近14日入境或近14日拟入境的情况
-#no need to check for 本人承诺
+# *sfyxjzxgym是否意向接种
+# *sfbyjzrq是否是不宜接种人群
+# *jzxgymqk当前接种情况
+# sffrqjwdg今日是否因发热请假未到岗（教职工）或未返校（学生）
+# sfqtyyqjwdg今日是否因发热外的其他原因请假未到岗（教职工）或未返校（学生）
+# tw今日是否有发热症状（高于37.2 ℃）
+# sfyqjzgc今日是否被当地管理部门要求在集中隔离点医学观察
+# sfcyglq今日是否居家隔离观察（居家非隔离状态填否）
+# sfcxzysx是否有任何与疫情相关的，值得注意的情况
+# sfsqhzjkk是否已经申领校区所在地健康码
+# sqhzjkkys今日申领校区所在地健康码的颜色
+# *sfzx今日是否在校
+# sfzgn所在地点
+# no need to check for 所在地点（请打开手机位置功能，并在手机权限设置中选择允许访问位置信息）
+# *sfymqjczrj本人家庭成员(包括其他密切接触人员)是否有近14日入境或近14日拟入境的情况
+# no need to check for 本人承诺
 option = [[] for i in range(6)]
 option[1] = ["sfyxjzxgym", "sfsqhzjkk","sqhzjkkys", "sfzgn"]
 option[2] = ["sffrqjwdg", "sfqtyyqjwdg", "tw", "sfyqjzgc", "sfcyglq", "sfcxzysx", "sfymqjczrj", "sfzx"]
-option[3] = ["jzxgymqk"]
-option[4] = []
+option[3] = []
+option[4] = ["jzxgymqk"]
 option[5] = ["sfbyjzrq"]
 ##########################
 
@@ -124,7 +124,7 @@ browser = webdriver.Chrome(options = options)
 browser.get("https://healthreport.zju.edu.cn/ncov/wap/default/index")
 browser.implicitly_wait(5)
 
-#Login
+# Login
 browser.find_element_by_name("username").send_keys(username)
 browser.find_element_by_name("password").send_keys(password)
 browser.find_element_by_id("dl").click()
@@ -135,7 +135,8 @@ except:
     browser.find_element_by_name("password").send_keys(password)
     browser.find_element_by_id("dl").click()
 
-#Report
+# Report
+browser.find_element_by_class_name("wapat-btn-ok").click()
 option[0] = ["sfqrxxss"] #Agreement at the end, no need to change
 for i in range(1, len(option)):
     for j in range(len(option[i])):
@@ -144,16 +145,27 @@ for i in range(1, len(option)):
 for i in range(len(option[0])):
     browser.find_element_by_name(option[0][i]).find_elements_by_tag_name("span")[0].click()
 
+# def getArea():
+#     selection = browser.find_element_by_name("area")
+#     selection.click()
+#     while True:
+#         if selection.find_element_by_tag_name("input").get_attribute("value") != "":
+#             if selection.find_element_by_tag_name("input").get_attribute("value").startswith('{"type":"error"'):
+#                 browser.find_element_by_class_name("wapat-btn-ok").click()
+#                 Select(browser.find_element_by_class_name("hcqbtn-danger")).select_by_value(province)
+#                 Select(browser.find_element_by_class_name("hcqbtn-warning")).select_by_value(city)
+#                 Select(browser.find_element_by_class_name("hcqbtn-primary")).select_by_value(area)
+#             break
+#         time.sleep(1)
+
 def getArea():
     selection = browser.find_element_by_name("area")
     selection.click()
     while True:
         if selection.find_element_by_tag_name("input").get_attribute("value") != "":
-            if selection.find_element_by_tag_name("input").get_attribute("value").startswith('{"type":"error"'):
-                browser.find_element_by_class_name("wapat-btn-ok").click()
-                Select(browser.find_element_by_class_name("hcqbtn-danger")).select_by_value(province)
-                Select(browser.find_element_by_class_name("hcqbtn-warning")).select_by_value(city)
-                Select(browser.find_element_by_class_name("hcqbtn-primary")).select_by_value(area)
+            break
+        if browser.find_element_by_class_name("wapat-title").get_attribute("textContent") == "获取位置信息失败，请检查定位服务是否开启！":
+            browser.find_element_by_class_name("wapat-btn-ok").click()
             break
         time.sleep(1)
 getArea()
@@ -165,5 +177,5 @@ try:
 except:
     pass
 
-#Close
+# Close
 browser.quit()
